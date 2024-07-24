ebc02c408ccf660dba6f112cf97a54aef1892a1c
Adam Strickland <astrickland@microsoft.com>
2024-07-24 15:40:23 +1000

    Add image tiling when the image is large

--- src/aiproxy/orchestration/agents/analyse_image_agent.py
+++ src/aiproxy/orchestration/agents/analyse_image_agent.py
@@ -1,4 +1,6 @@
 from typing import Callable
+from PIL import Image
+
 
 from aiproxy import ChatContext, ChatResponse
 from aiproxy.data import ChatMessage
@@ -42,9 +44,8 @@ class AnalyseImageAgent(Agent):
 
         ## If the msg is bytes, then base64 it, otherwise assume it's already base64'd
         if isinstance(message, bytes):
-            import base64
-            message = base64.b64encode(message).decode('utf-8')
-        
+            return self.process_native_image(message, context)
+           
         # Check with the context if the it knows the extension of the image
         img_ext = context.get_metadata("image-extension", self._default_image_extension)
     
@@ -55,7 +56,7 @@ class AnalyseImageAgent(Agent):
                 "type": "image_url",
                 "image_url": {
                     "url": f'data:image/{img_ext};base64,{message}',
-                    "detail": "low"
+                    "detail": "high"
                 }
             }, 
             {
@@ -70,3 +71,102 @@ class AnalyseImageAgent(Agent):
             self._isolated_thread_id = context.thread_id
 
         return response
+    
+    def process_native_image(self, message:bytes, context:ChatContext) -> ChatResponse: 
+        import io
+        import base64
+        import math
+
+        content = []
+        if context.get_metadata('slice-image', 'true') == 'true':
+            ## Load the image
+            image = Image.open(io.BytesIO(message))
+            
+            # If the image is larger than 512x512, break it up into smaller images
+            tiles = []
+            if image.width >  1536 or image.height > 1536:
+                # Break up the image into smaller images (overlapping each image by ~30%)
+                desired_width = 1024
+                desired_height = 1024
+                cols = math.ceil(image.width / desired_width * 1.33)
+                rows = math.ceil(image.height / desired_height * 1.33)
+                if cols * rows > 10: 
+                    cols = 3
+                    rows = 3
+                    desired_height = image.height // rows
+                    desired_width = image.width // cols
+                
+                for i in range(int(cols)):
+                    for j in range(int(rows)):
+                        left = int(i * desired_width * 0.7)
+                        upper = int(j * desired_height * 0.7)
+
+                        right = left + desired_width
+                        lower = upper + desired_height
+                        if right > image.width: 
+                            right = image.width
+                            left = right - desired_width
+                        if lower > image.height: 
+                            lower = image.height
+                            upper = lower - desired_height
+
+                        tile = image.crop((left, upper, right, lower))
+                        tiles.append(tile)
+            else: 
+                tiles = [image]
+
+            
+            # Check with the context if the it knows the extension of the image
+            img_ext = context.get_metadata("image-extension", image.format or self._default_image_extension)
+        
+            for tile in tiles:
+                # Convert the image to base64
+                buffered = io.BytesIO()
+                tile.save(buffered, format=img_ext)
+                img_str = base64.b64encode(buffered.getvalue()).decode()
+                content.append({
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f'data:image/{img_ext};base64,{img_str}',
+                        "detail": "high"
+                    }
+                })
+            
+            content.append({
+                "type": "text",
+                "text": f"Here are {len(tiles)} images that are overlapping cutouts from a single large image. {self._analyse_prompt or 'Process these images'}"
+            })
+
+            context.set_metadata("image-tiles", len(tiles))
+        else:
+             # Check with the context if the it knows the extension of the image
+            img_ext = context.get_metadata("image-extension", self._default_image_extension)
+            img_str = base64.b64encode(message).decode()
+            content.append({
+                "type": "image_url",
+                "image_url": {
+                    "url": f'data:image/{img_ext};base64,{img_str}',
+                    "detail": "high"
+                }
+            })
+
+            content.append({
+                "type": "text",
+                "text": self._analyse_prompt or 'Process this image'
+            })
+
+
+
+        # Send message to the proxy
+        img_msg = ChatMessage(
+            role="user", 
+            content=content,
+        )
+        context.add_message_to_history(img_msg)
+        response = self.proxy.send_message(None, context, override_model=self._custom_model, override_system_prompt=self._custom_system_prompt, function_filter=self._function_filter)
+        # If the agent is using an isolated thread, store the thread-id for use later 
+        if self._thread_isolated:
+            self._isolated_thread_id = context.thread_id
+
+        return response
+    
